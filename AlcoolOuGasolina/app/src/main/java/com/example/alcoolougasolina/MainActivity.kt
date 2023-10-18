db391dbdf76ffa08257d03a3d353d0be224dc24f
MauroV27 <maurovictor13@gmail.com>
2023-10-18 15:30:52 -0300

    BUG FIXED - Agora o aplicativo funciona em modo landscape e portrait.

--- AlcoolOuGasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ AlcoolOuGasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.alcoolougasolina
 
+//import android.R
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,6 +12,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alcoolougasolina.R
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +26,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.renderScreen()
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("PDM23","No onResume, $percentual")
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d("PDM23","No onStart")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("PDM23","No onPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("PDM23","No onStop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("PDM23","No onDestroy")
+    }
+
+    private fun saveStatus(value : Boolean ){
+        val sharerPreference : SharedPreferences = getSharedPreferences("SharedPercentage", Context.MODE_PRIVATE)
+        val editData = sharerPreference.edit()
+
+        editData.apply{
+            putBoolean("active", value)
+        }.apply()
+    }
+
+    private fun loadStatus() : Boolean {
+        val sharerPreference : SharedPreferences = getSharedPreferences("SharedPercentage", Context.MODE_PRIVATE)
+
+        return sharerPreference.getBoolean("active", false)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // /res/layout-land
+            setContentView(R.layout.activity_main_land)
+
+        }else { // (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
+            // /res/layout
+            setContentView(R.layout.activity_main)
+        }
+
+        this.renderScreen()
+    }
+
+    private fun renderScreen(){
         // Code for read data in sharedPrefereces about percentual
         this.isSwitchActive = loadStatus()
         this.percentual = if ( this.isSwitchActive ) 0.75 else 0.7
@@ -73,45 +136,5 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-    }
-
-    override fun onResume(){
-        super.onResume()
-        Log.d("PDM23","No onResume, $percentual")
-    }
-
-    override fun onStart(){
-        super.onStart()
-        Log.d("PDM23","No onStart")
-    }
-
-    override fun onPause(){
-        super.onPause()
-        Log.d("PDM23","No onPause")
-    }
-
-    override fun onStop(){
-        super.onStop()
-        Log.d("PDM23","No onStop")
-    }
-
-    override fun onDestroy(){
-        super.onDestroy()
-        Log.d("PDM23","No onDestroy")
-    }
-
-    private fun saveStatus(value : Boolean ){
-        val sharerPreference : SharedPreferences = getSharedPreferences("SharedPercentage", Context.MODE_PRIVATE)
-        val editData = sharerPreference.edit()
-
-        editData.apply{
-            putBoolean("active", value)
-        }.apply()
-    }
-
-    private fun loadStatus() : Boolean {
-        val sharerPreference : SharedPreferences = getSharedPreferences("SharedPercentage", Context.MODE_PRIVATE)
-
-        return sharerPreference.getBoolean("active", false)
     }
 }
