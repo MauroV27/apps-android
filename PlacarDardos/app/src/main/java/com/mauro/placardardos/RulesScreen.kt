4c844d2e71cdada50836aac62f97bb6e0b3c84ab
MauroV27 <maurovictor13@gmail.com>
2023-11-06 10:48:43 -0300

    Create content in rules screen.

--- PlacarDardos/app/src/main/java/com/mauro/placardardos/RulesScreen.kt
+++ PlacarDardos/app/src/main/java/com/mauro/placardardos/RulesScreen.kt
@@ -1,11 +1,20 @@
 package com.mauro.placardardos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class RulesScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rules_screen)
+
+        // Back to home screen
+        val icBtnBackHome = findViewById<ImageButton>(R.id.imgBtnBackHome)
+        icBtnBackHome.setOnClickListener{
+            val intentGoHome = Intent(this, MainActivity::class.java).apply{}
+            startActivity(intentGoHome)
+        }
     }
 }
